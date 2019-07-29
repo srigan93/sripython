a478a31b503b6e744a71de656c24e626e71c250c
srigan93 <43399167+srigan93@users.noreply.github.com>
2019-07-29 12:18:49 +0530

    Update string_opns.py

--- string_opns.py
+++ string_opns.py
@@ -1,1 +1,13 @@
-
+#sample program for python string operations
+string1="hello programmers"
+string2='welcome to the world of programming'
+string3='''this is a demo for python strings'''
+print(string1)
+print(string1[1:])
+print(string1[1:6])
+print(string1[-2])
+print(string1[-2:7])
+print(string1.index("o"))
+print(string1+string2)
+print(string1[1:8:2]+string2[:4])
+print(::-1)
