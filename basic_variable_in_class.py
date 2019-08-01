f6a38bff9bfcb0303c5e69af2dd1f88373e13fc9
srigan93 <43399167+srigan93@users.noreply.github.com>
2019-08-01 11:18:40 +0530

    Create basic_variable_in_class.py

---
+++ basic_variable_in_class.py
@@ -0,0 +1,14 @@
+class ABC: #Just let pass the statemants
+    pass
+
+p1 = Point() # two Objects p1, p2 for class ABC 
+p2 = Point()
+
+p1.a = 5
+p1.b = 4
+
+p2.c = 3
+p2.d = 6
+
+print(p1.x, p1.y) #print the values
+print(p2.x, p2.y)
