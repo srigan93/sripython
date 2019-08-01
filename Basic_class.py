bd6ca257561e7cc3f0f3d08e35a6156c9fd8dc5c
srigan93 <43399167+srigan93@users.noreply.github.com>
2019-08-01 11:13:32 +0530

    Create Basic_class.py

---
+++ Basic_class.py
@@ -0,0 +1,8 @@
+#!/bin/python
+class MyFirstClass:
+    pass  #Just pass the values
+
+a = MyFirstClass() #Creates a object for the class
+b = MyFirstClass() #Created b object for the class
+print(a) 
+print(b)
